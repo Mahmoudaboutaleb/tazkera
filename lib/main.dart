314852b7954dc49ac7da74443199004c6d725d5f
Ahmad.Alnahrawy <37123037+Ahmednahrawy@users.noreply.github.com>
2024-09-15 01:31:12 +0300

    fix .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tazkera/features/Auth/login_page.dart';
-import 'package:tazkera/features/home-screen/home_page.dart';
 
 void main() {
   runApp(const MyApp());
