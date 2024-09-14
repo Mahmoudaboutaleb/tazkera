a4e6ed6c2af6b5a0e1f4ac663d56dc9341ed3c04
Ahmad.Alnahrawy <37123037+Ahmednahrawy@users.noreply.github.com>
2024-09-15 02:22:29 +0300

    add colors file

---
+++ lib/config/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color green = Color.fromRGBO(76, 174, 159, 1.0);
+  static const Color darkerGreen = Color.fromRGBO(76, 174, 159, 0.3);
+}
